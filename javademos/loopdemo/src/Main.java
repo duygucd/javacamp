74e861bac8aa69e7857b5fdef633927206a58172
duygu <101789771+duygucd@users.noreply.github.com>
2022-09-20 21:14:41 +0300

    Add files via upload

---
+++ javademos/loopdemo/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        //for
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println(" For Döngüsü bitti");
+
+
+        //while
+        int i = 2;
+        while (i < 10) {
+
+            System.out.println(i);
+            i += 2;
+        }
+        System.out.println("While Döngüsü bitti");
+        // Do -while
+        int j=1;
+        do {
+            System.out.println(j);
+            i += 2;
+
+        }while(i<10);
+        System.out.println(" Do-While Döngüsü bitti");
+        }
+
+    }
+
+
